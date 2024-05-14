353c5269a5d88ab8484e0fedb1906ac00683c4bc
Ben Horner <ben.horner@quantspark.com>
2024-05-14 21:52:59 +0100

    feat: add theme and font

---
+++ src/themes/DefaultTheme.tsx
@@ -0,0 +1,50 @@
+import { createTheme } from "@mui/material/styles";
+
+declare module "@mui/material/styles" {
+  interface Palette {
+    portfolio: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    portfolio?: PaletteOptions["primary"];
+  }
+}
+
+export const DefaultTheme = createTheme({
+  typography: {
+    fontFamily: [
+      "Jet Brains Mono",
+      "-apple-system",
+      "BlinkMacSystemFont",
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      "sans-serif",
+    ].join(","),
+  },
+  palette: {
+    portfolio: {
+      main: "#FFF",
+      contrastText: "#D71009",
+    },
+    primary: {
+      main: "#FFF",
+    },
+    secondary: {
+      main: "#F6F7F9",
+    },
+    error: {
+      main: "#D7110A",
+      dark: "#FFCCC3",
+    },
+    warning: {
+      main: "#ffa726",
+      dark: "#FFDAB8",
+    },
+  },
+});
