3b1ddadde6a418dd0cc3a0c2a2e7f9d713060352
Ben Horner <ben.horner@quantspark.com>
2024-05-14 21:55:19 +0100

    feat: setup page routes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
+import { Layout } from "./Layout";
 import { StoreProvider } from "./StoreProvider";
 import "./globals.css";
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <StoreProvider>
-      <Component {...pageProps} />
-    </StoreProvider>
+    <Layout>
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </Layout>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+import { Hero } from "@/components/organisms/Hero/Hero";
+import "animate.css";
+import Head from "next/head";
+
+export default function Index() {
+  return (
+    <>
+      <Head>
+        <title>Ben Horner&apos;s Portfolio</title>
+      </Head>
+      <Hero />
+    </>
+  );
 }
