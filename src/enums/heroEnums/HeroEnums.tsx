67e196864467b4592b1b5b545ca4b034d470e845
Ben Horner <ben.horner@quantspark.com>
2024-05-14 21:57:42 +0100

    feat: add hero page

---
+++ src/enums/heroEnums/HeroEnums.tsx
@@ -0,0 +1,14 @@
+export const FULL_SCREEN_SITE_TEXT = `
+I am a full stack developer, specialising in NLP, with a mission to create elegant apps 
+that are reliable, maintainable and scalable. 
+My stack of choice is Flask or Django for the backend, 
+React for the frontend with MySql for the database but I am always learning new technologies.
+Please feel free to explore my previous work and contact me if you have any questions:
+`;
+
+export const MOBILE_SCREEN_SITE_TEXT = `
+I am a full stack developer, specialising in NLP, with a mission to create elegant apps 
+that are reliable, maintainable and scalable. 
+`;
+
+export const SUB_HEADER = "Hello, I'm Ben...";
