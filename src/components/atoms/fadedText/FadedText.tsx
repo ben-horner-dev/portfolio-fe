a88e90108d6c42bbe90070f60ddb80cae2692001
GitHub Ben Horner Dev <info@benhorner.dev>
2024-05-27 09:55:20 +0100

    feat: add faded text atom

---
+++ src/components/atoms/fadedText/FadedText.tsx
@@ -0,0 +1,23 @@
+import styles from "./FadedText.module.css";
+
+interface FadedTextProps {
+  text: string;
+  textClass: string;
+  handleAnimationEnd: () => void;
+}
+
+export const FadedText = ({
+  text,
+  textClass,
+  handleAnimationEnd,
+}: FadedTextProps) => {
+  return (
+    <div
+      className={`${textClass} ${styles["faded-text"]}`}
+      draggable="false"
+      onAnimationEnd={handleAnimationEnd}
+    >
+      {text}
+    </div>
+  );
+};
