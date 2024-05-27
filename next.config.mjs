959fd1227a4ab93c3c98832ee8daa5acd4469047
GitHub Ben Horner Dev <info@benhorner.dev>
2024-05-27 19:07:04 +0100

    fix: revert docker fix and apply webpack fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = { output: "standalone" };
+const nextConfig = {
+    output: "standalone",
+    webpack: (config, { isServer }) => {
+        const __dirname = dirname(fileURLToPath(import.meta.url));
+        config.resolve.alias["@"] = join(__dirname, "src");
+        return config;
+    },
+};
 
 export default nextConfig;
