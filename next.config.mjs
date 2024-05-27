76ce98cf88dda4cdf609bd58f523ec1cadabe879
GitHub Ben Horner Dev <info@benhorner.dev>
2024-05-27 19:12:44 +0100

    fix: dont set env vars and revert config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "standalone",
-    webpack: (config, { isServer }) => {
-        const __dirname = dirname(fileURLToPath(import.meta.url));
-        config.resolve.alias["@"] = join(__dirname, "src");
-        return config;
-    },
-};
+const nextConfig = { output: "standalone" };
 
 export default nextConfig;
