030447c9568624727bff9435d14a1be38df3893b
GitHub Ben Horner Dev <info@benhorner.dev>
2024-07-16 22:47:47 +0100

    refactor: toolbar to use state to change icons

--- src/components/organisms/customToolbar/CustomToolbar.tsx
+++ src/components/organisms/customToolbar/CustomToolbar.tsx
@@ -1,11 +1,20 @@
+import { ShoppingCart } from "@/components/atoms/shoppingCart/ShoppingCart";
 import { UserAvatar } from "@/components/atoms/userAvatar";
 import { BtnGroup } from "@/components/molecules/btnGroup";
 import { Burger } from "@/components/molecules/burger";
 import { LogoBtn } from "@/components/molecules/logoBtn/LogoBtn";
+import { NavBarIconsEnums } from "@/enums/navBarIconEnums";
+import { useAppSelector } from "@/hooks";
 import { Toolbar } from "@mui/material";
 import Box from "@mui/material/Box";
 
 export const CustomToolbar = () => {
+  const icon = useAppSelector((state) => state.navBarIcons.icon);
+  const btnGroupMap = {
+    [NavBarIconsEnums.USER]: <UserAvatar />,
+    [NavBarIconsEnums.SHOPPING_CART]: <ShoppingCart />,
+  };
+
   return (
     <Toolbar data-testid="custom-toolbar">
       <Box className="md:block hidden">
@@ -17,9 +26,7 @@ export const CustomToolbar = () => {
         <LogoBtn />
       </Box>
       <Box className="md:block hidden">
-        <BtnGroup>
-          <UserAvatar />
-        </BtnGroup>
+        <BtnGroup>{icon !== null && btnGroupMap[icon]}</BtnGroup>
       </Box>
     </Toolbar>
   );
