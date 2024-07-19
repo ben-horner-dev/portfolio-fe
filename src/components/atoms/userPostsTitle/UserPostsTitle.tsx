0dcc1bdfc79926913d6947d591420bf10a8b7acd
GitHub Ben Horner Dev <info@benhorner.dev>
2024-07-19 19:44:39 +0100

    feat: add user posts title

---
+++ src/components/atoms/userPostsTitle/UserPostsTitle.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material";
+
+export const UserPostsTitle = () => {
+  return (
+    <Typography variant="h6" gutterBottom component="div">
+      Posts:
+    </Typography>
+  );
+};
