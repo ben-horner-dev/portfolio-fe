623fdbaf45979e3cfffb5eb52e9d19ffd83d047e
Ben Horner <info@benhorner.dev>
2024-07-21 17:49:52 +0100

    Bugfix/show responsive menu icons (#21)
    * fix: fixing subdir casing
    
    * Revert "Merge branch 'main' of github.com:ben-horner-dev/portfolio-fe"
    
    This reverts commit 8592ef68c433f595e4fab2eb889949188ee4ad46, reversing
    changes made to e310902c16fbb3d65a905f81af0be0d7813b6997.
    
    * fix: rm old tracked upper case dirs
    
    * fix: show responsive menu icons when screen below 750
    
    * fix: when screen x < 750 hide x overflow and stack  footer
    
    * feat: add meta required links
    
    * fix: fix e2e tests btn count
    
    * feat: add terms and conditions, privacy and deletion text

--- cypress/e2e/eCommercePayment.cy.ts
+++ cypress/e2e/eCommercePayment.cy.ts
@@ -31,7 +31,7 @@ context("Actions", () => {
         cy.get('[data-testid="product-btn"]').first().click({ force: true });
         cy.get('[data-testid="product-btn"]').first().click({ force: true });
         cy.get(".MuiBadge-badge").contains("2");
-        cy.get('[data-testid="shopping-cart-btn"]').click();
+        cy.get('[data-testid="shopping-cart-btn"]').last().click();
         cy.get(".MuiTableCell-body").should("have.length", 6);
         cy.get(".MuiTableCell-body").first().should("have.text", "Product 1");
         cy.get(".MuiTableCell-body").eq(1).should("have.text", "$2");
---
+++ src/pages/deletion.tsx
@@ -0,0 +1,10 @@
+export default function Privacy() {
+  return (
+    <>
+      No data is collected from the user. Your name is collected locally by the
+      application when you log in but is not sent to our server and will be
+      deleted when you clear your local cache. To contact us regarding data
+      deletion email us on info@benhorner.dev
+    </>
+  );
+}
