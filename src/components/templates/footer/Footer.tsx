c22084b5be5508b412db450c04c2468a6a0b6b01
GitHub Ben Horner Dev <info@benhorner.dev>
2024-07-21 17:10:15 +0100

    fix: when screen x < 750 hide x overflow and stack  footer

--- src/components/templates/footer/Footer.tsx
+++ src/components/templates/footer/Footer.tsx
@@ -1,6 +1,7 @@
 import { GithubIcon } from "@/components/atoms/githubIcon";
 import { LinkedInIcon } from "@/components/atoms/linkedInIcon";
 import { useRefs } from "@/contexts/refProvider";
+import { Grid } from "@mui/material";
 import Box from "@mui/material/Box";
 import style from "./Footer.module.css";
 
@@ -14,12 +15,18 @@ export const Footer = () => {
       className={`${style.container} snap-section`}
       ref={contactRef}
     >
-      <a href="https://github.com/ben-horner-dev/portfolio" target="_blank">
-        <GithubIcon />
-      </a>
-      <a href="https://www.linkedin.com/in/ben-horner-dev/" target="_blank">
-        <LinkedInIcon />
-      </a>
+      <Grid container>
+        <Grid item xs={12} display={"flex"} justifyContent={"center"}>
+          <a href="https://github.com/ben-horner-dev/portfolio" target="_blank">
+            <GithubIcon />
+          </a>
+        </Grid>
+        <Grid item xs={12} display={"flex"} justifyContent={"center"}>
+          <a href="https://www.linkedin.com/in/ben-horner-dev/" target="_blank">
+            <LinkedInIcon />
+          </a>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
